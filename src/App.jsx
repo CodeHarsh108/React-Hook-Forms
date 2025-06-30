42fe8308999722a4d63fb01e0bcb1596bdf453d7
Harsh Patil <itsharshhh6@gmail.com>
2025-06-30 10:24:39 +0530

    Created a basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import './App.css';
+
 
 function App() {
+  const handleSubmit = () => {
+
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
     <div>
       <h1>Forms in react</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name :
+          <input type="text" name='name'  onChange={handleChange}/> 
+        </label>
+      </form>
     </div>
   )
 }
