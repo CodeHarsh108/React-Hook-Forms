e4271927a48b64f739f2a948d0cf671654b585aa
Harsh Patil <itsharshhh6@gmail.com>
2025-07-01 11:30:31 +0530

    Basic forms problems

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,35 @@ import { useState } from 'react';
 
 function App() {
   const [formData, setFormData] = useState({name: '', email: ''});
+  const [errors, setErrors] = useState({});
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+    return newErrors;
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form submitted with data:", formData);
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    }else{
+      console.log("Form submitted with data:", formData);
+    }
+    
   };
 
   const handleChange = (e) => {
+    const {name, value} = e.target;
     setFormData(
       {...formData, [e.target.name]: e.target.value, } 
     );
+    if(errors[name]){
+      const newErrors = {...errors};
+      delete newErrors[name];
+      setErrors(newErrors);
+    }
   };
 
   return (
@@ -22,6 +42,7 @@ function App() {
         <label>
           Name :
           <input type="text" name='name' value={formData.name} onChange={handleChange}/> 
+          {errors.name && <span className="error">{errors.name}</span>}
         </label>
         <br />
         <label>
