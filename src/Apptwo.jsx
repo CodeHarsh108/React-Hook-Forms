b71abe5862018846c8239911b7e180e03b104881
Harsh Patil <itsharshhh6@gmail.com>
2025-07-03 09:50:55 +0530

    Watching Values in React Hook Form

--- src/Apptwo.jsx
+++ src/Apptwo.jsx
@@ -1,9 +1,20 @@
+import { use, useEffect } from 'react';
 import './App.css';
 import { useForm } from 'react-hook-form';
 
 function Apptwo() {
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, watch} = useForm();
     const onSubmit = (data) => console.log(data);
+    const watchedName = watch('name');
+    const watchedEmail = watch('email');
+
+    useEffect(() => {
+    console.log('Name changed:', watchedName);
+    },[watchedName]);
+
+    useEffect(() => {
+    console.log('Email changed:', watchedEmail);}, [watchedEmail]);
+    
   return (
     <div>
       <h1>Forms in react</h1>
